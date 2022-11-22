8fe28ffb583f5d7580a4a93f1cda3142d7d314b6
Kyle Schoppmann <klparuu@yahoo.com>
2022-11-22 12:28:03 -0700

    Added functions to generate random password with criteria chosen by user

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacter = [".", ",", "/", "?", "!", "@", "#", "$", "%", "&", "'"];
+
+function getPasswordLength() {
+  var passwordLength = prompt("Choose a password length from 8 to 128.")
+  parseInt(passwordLength)
+  if(passwordLength > 128) {
+    alert("Choose a lenght less then 129.");
+  } else if(passwordLength < 8) {
+    alert("Choose a length greater then 7.");
+  } else {
+    return passwordLength;
+  }
+}
 
 function getPasswordCriteria() {
-  var userChoseNumbers = confirm("Would you like to add numbers?");
+  var passwordLength = getPasswordLength();
+  var userChoseNumbers = confirm("Would you like to include numbers?");
+  var userChoseUpperCase = confirm("Would you like to include uppercase letters?");
+  var userChoseLowerCase = confirm("Would you like to include lowercase letters?");
+  var userChoseSpecial = confirm("Would you like to include special characters?");
   var options = {
-    userChoseNumbers: userChoseNumbers
+    passwordLength: passwordLength,
+    userChoseNumbers: userChoseNumbers,
+    userChoseUpperCase: userChoseUpperCase,
+    userChoseLowerCase: userChoseLowerCase,
+    userChoseSpecial: userChoseSpecial
   };
   return options;
 }
@@ -14,47 +37,27 @@ function createRandom(length) {
 }
 
 function generatePassword() {
-  var userChose = getPasswordCriteria();
-  var availableChars = [];
-  var passwordArr = [];
-  if(userChose.userChoseNumbers) {
-    availableChars = availableChars.concat(numbers);
-   
-  }
+    var userChose = getPasswordCriteria();
+    var availableChars = [];
+    var passwordArr = [];
+    if(userChose.userChoseNumbers) {
+      availableChars = availableChars.concat(numbers);
+    }
+    if(userChose.userChoseUpperCase) {
+      availableChars = availableChars.concat(upperCase);
+    }
+    if(userChose.userChoseLowerCase) {
+      availableChars = availableChars.concat(lowerCase);
+    }
+    if(userChose.userChoseSpecial) {
+      availableChars = availableChars.concat(specialCharacter);
+    }
 
-  for(var i = 0; i < 4; i++) {
-    passwordArr.push(availableChars[createRandom(availableChars.lenght)])
-  }
-
-  return passwordArr.join("")
-
-  /* var myNewPassword = "";
- 
-  var passwordLength = prompt("Choose a password length from 8 to 128.")
-  console.log(passwordLength)
-  parseInt(passwordLength)
-
-  if(passwordLength > 128) {
-    alert("Choose a lenght less then 129.");
-  } else if(passwordLength < 8) {
-    alert("Choose a length greater then 7.");
-  } else {
-    console.log(passwordLength)
-  }
-
-  var lowercase = confirm("Would you like to include lowercase letters?")
-    console.log(lowercase)
-
-  var uppercase = confirm("Would you like to include uppercase letters?")
-    console.log(uppercase)
-
-
-  var specialCharacter = confirm("Would you like to include special characters?")
-    console.log(specialCharacter)
-
-  
-
-  return myNewPassword; */
+    for(var i = 0; i < userChose.passwordLength; i++) {
+      passwordArr.push(availableChars[createRandom(availableChars.length)])
+    }
+    
+  return passwordArr.join("");
 }
 
 // Assignment Code
